0670bc22496438d600116872f178fce8bdbc36c4
xetra11 <xetra11@outlook.com>
2018-10-05 19:28:01 +0200

    feature(command): add termbin push command for region

--- termin-minor-mode.el
+++ termin-minor-mode.el
@@ -1,2 +1,16 @@
-(+ 3 5)
+(defun region-to-termbin (start end)
+  "deliver marked region to termin via shell command"
+  (interactive "r")
+  (shell-command-on-region start end "nc termbin.com 9999")
+)
+
+
+(define-minor-mode termbin-mode
+  "This mode will allow you to push your buffer content directly to termin.com"
+  :lighter " termbin"
+  :keymap (let ((map (make-sparse-keymap)))
+            (define-key map (kbd "C-c t r") 'region-to-termbin)
+            map)
+)
+
 
