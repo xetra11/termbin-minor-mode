88fada65e0f9c60989ed0f0708ad2a383b15edaf
xetra11 <xetra11@outlook.com>
2018-10-06 23:36:52 +0200

    feature(buffer): add termbin push command for whole buffer

--- termin-minor-mode.el
+++ termin-minor-mode.el
@@ -5,12 +5,19 @@
   (shell-command-on-region start end "nc termbin.com 9999")
 )
 
+(defun buffer-to-termbin ()
+  "push the whole buffer to termin.com via shell command"
+  (interactive)
+  (message "pushing buffer to termbin.com...")
+  (shell-command-on-region (point-min) (point-max) "nc termbin.com 9999")
+  )
 
 (define-minor-mode termbin-mode
   "This mode will allow you to push your buffer content directly to termin.com"
   :lighter " termbin"
   :keymap (let ((map (make-sparse-keymap)))
             (define-key map (kbd "C-c t r") 'region-to-termbin)
+            (define-key map (kbd "C-c t b") 'buffer-to-termbin)
             map)
 )
 
