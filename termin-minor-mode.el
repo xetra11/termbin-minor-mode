80ff73f0399c3f1f4f356d8f0fcd06170fd216e4
xetra11 <xetra11@outlook.com>
2018-10-06 23:38:50 +0200

    refactor(description): fix typos

--- termin-minor-mode.el
+++ termin-minor-mode.el
@@ -1,19 +1,19 @@
 (defun region-to-termbin (start end)
-  "push the marked region to termin.com via shell command"
+  "push the marked region to termbin.com via shell command"
   (interactive "r")
   (message "pushing region to termbin.com...")
   (shell-command-on-region start end "nc termbin.com 9999")
 )
 
 (defun buffer-to-termbin ()
-  "push the whole buffer to termin.com via shell command"
+  "push the whole buffer to termbin.com via shell command"
   (interactive)
   (message "pushing buffer to termbin.com...")
   (shell-command-on-region (point-min) (point-max) "nc termbin.com 9999")
   )
 
 (define-minor-mode termbin-mode
-  "This mode will allow you to push your buffer content directly to termin.com"
+  "This mode will allow you to push your buffer content directly to termbin.com"
   :lighter " termbin"
   :keymap (let ((map (make-sparse-keymap)))
             (define-key map (kbd "C-c t r") 'region-to-termbin)
